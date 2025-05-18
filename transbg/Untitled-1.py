7a08f5305902b50f988dcd2e658b8623701a1eff
Umaimah <umaimahali07@gmail.com>
2025-05-18 12:55:41 +0500

    INTRO TO PYTHON

---
+++ transbg/Untitled-1.py
@@ -0,0 +1,20 @@
+name = "Umaimah"
+age = 11
+is_student = True
+weight = 38.5
+
+print("Name:", name)
+print("Data Type of Name is", type(name))
+print("Age :", age)
+print("Data Type of Name is", type(age))
+print("is_student :", is_student)
+print("Data Type for is_student is", type(is_student))
+print("Weight :", weight)
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
