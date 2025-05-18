a6251fc5477b1d1c19f406fec949179f63ddcadc
Umaimah <umaimahali07@gmail.com>
2025-05-18 14:52:43 +0500

    Python homework

---
+++ python/homeworkpython.py
@@ -0,0 +1,6 @@
+sentence1 = input("my name is Umaimah: ")
+print("You entered (1):", sentence1)  
+sentence2 = input("and i am a student at codingal: ")
+print("You entered (2):", sentence2)  
+combined_sentence = sentence1 + "" + sentence2
+print("Combined Sentence:", combined_sentence)
