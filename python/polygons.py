ce7319db8c3b4ccd731f9c70742f8a0f177be10e
Umaimah <umaimahali07@gmail.com>
2025-05-24 15:46:46 +0500

    polygons with turtle

---
+++ python/polygons.py
@@ -0,0 +1,22 @@
+import turtle 
+turtle.Screen().bgcolor("pink")
+board = turtle.Turtle()
+
+#triangle
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+#square
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+
+
+turtle.done()
