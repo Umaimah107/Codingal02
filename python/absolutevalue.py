adf5a5dc566f2e3036005e56d19247dbd4fda16a
Umaimah <umaimahali07@gmail.com>
2025-06-01 12:54:03 +0500

    functions

---
+++ python/absolutevalue.py
@@ -0,0 +1,8 @@
+def absolute_value(num):
+    if num >= 0:
+        return num
+    else:
+        return-num
+    
+print("Absolute Value of 11 :", absolute_value(11))
+print("Absolute Value of -90 :", absolute_value(-90))
