943acf47fc8bf2f0c29644357793e803fe49f23b
Umaimah <umaimahali07@gmail.com>
2025-06-08 08:07:12 +0500

    student report

---
+++ python/student.py
@@ -0,0 +1,10 @@
+class Fifth:
+    subjs=["eng","maths","Sci"]
+
+    def getMarks(self):
+        m= int(input())
+        return m 
+Umaimah = Fifth()
+print(Umaimah.subjs) 
+m1 = Umaimah.getMarks()
+print(m1)   
