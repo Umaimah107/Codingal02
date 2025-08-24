35510d88d26adb5bfd4d080d0d5de9e28fe47902
Umaimah <umaimahali07@gmail.com>
2025-08-24 13:00:35 +0500

    intro to pygame

---
+++ SQL practice/into to sql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE students (
+    id INT PRIMARY KEY,
+    name VARCHAR(50)
+    student_age INTEGER
+);
+
+INSERT INTO students (id, name, age) VALUES (275, 'lily', 8)
+
+SELECT * FROM students;
