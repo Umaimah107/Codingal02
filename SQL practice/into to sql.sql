dc941bc2e1ef8015617ce5f2712d22e052ef4970
Umaimah <umaimahali07@gmail.com>
2025-08-31 12:54:07 +0500

    sprites

--- SQL practice/into to sql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE students (
-    id INT PRIMARY KEY,
-    name VARCHAR(50)
-    student_age INTEGER
-);
-
-INSERT INTO students (id, name, age) VALUES (275, 'lily', 8)
-
-SELECT * FROM students;
