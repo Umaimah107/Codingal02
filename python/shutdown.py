b88d33c6d2d8738ba4de83b9081d6ae424a449e8
Umaimah <umaimahali07@gmail.com>
2025-10-12 15:33:15 +0500

    SHUTDOWN

--- python/shutdown.py
+++ python/shutdown.py
@@ -2,8 +2,17 @@ import os
 import time
 
 def shutdown():
-    choice = input("Do you want to shut down your computer? (yes/no)").strip().lower()
+    choice = input("Do you want to shut down your computer? (yes/no): ").strip().lower()
 
     if choice == "yes":
-        print("Shutting down in five seconds....")
-        time
+        print("Shutting down in 5 seconds...")
+        time.sleep(5)
+        os.system("shutdown /s /t 1")
+
+    elif choice == "no":
+        print("Shutdown aborted.")
+
+    else:
+        print("Sorry, this is an invalid response.")
+
+shutdown()
