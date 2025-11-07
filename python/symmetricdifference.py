ced8a20efade8bf705d5ff0373190936134c21d4
Umaimah <umaimahali07@gmail.com>
2025-11-07 16:51:28 +0500

    SYMETRICDIFFERENCE

---
+++ python/symmetricdifference.py
@@ -0,0 +1,8 @@
+setu = {11,22,33,44,55}
+seta = {99,110,22,77,33}
+print("Original set elements: ")
+print(setu)
+print(seta)
+print("Symmetrical difference of two said sets:")
+setz = setu.symmetric_difference(seta)
+print(setz)
