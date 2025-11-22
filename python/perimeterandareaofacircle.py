d3cfc6431b22acdb05f6cb6a5d7e2b8d8a1c362f
Umaimah <umaimahali07@gmail.com>
2025-11-22 08:49:09 +0500

    homework

---
+++ python/perimeterandareaofacircle.py
@@ -0,0 +1,12 @@
+class Circle():
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return 3.14 *self.radius* self.radius
+    def perimeter(self):
+        return 2 * 3.14 * self.radius 
+r = float(input("Enter the radius of the circle: "))
+c = Circle(r) 
+print("Area of the circle is: ", c.area())
+print("Perimeter of the circle is: ", c.perimeter())
+  
