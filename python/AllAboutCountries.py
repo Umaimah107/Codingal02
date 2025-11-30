a88c1b9c8770f270b539c4746dcb5dbf500bc279
Umaimah <umaimahali07@gmail.com>
2025-11-30 12:36:06 +0500

    ALL ABOUT COUNTRIES

---
+++ python/AllAboutCountries.py
@@ -0,0 +1,19 @@
+class Pakistan():
+    def capital(self):
+        print("The capital of Pakistan is Islamabad")
+    
+    def language(self):
+        print("Urdu is the most widely spoken language of Pakistan")
+    
+    def type(self):
+        print("Pakistan is a developing country")
+
+class Australia():
+    def capital(self):
+        print("The capital of Australia is Caberra")
+
+    def language(self):
+        print("English is the most widely spoken language in Australia")
+        
+    def type(self):
+        print("Austalia is a developed country")
