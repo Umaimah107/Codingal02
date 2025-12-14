ef10bd137c2896bd6295a6498a67d456081c18c3
Umaimah <umaimahali07@gmail.com>
2025-12-14 12:27:59 +0500

    ASYMPTOTIC NOTATIONS

---
+++ python/O(n)Time.py
@@ -0,0 +1,10 @@
+def Ontime(n):
+    iteration= 0
+    for i in range(1, n+1):
+        iteration+=1
+    print("When n is ",n," Iterations =  ", iteration)
+
+Ontime(10)
+Ontime(20)
+Ontime(45)
+print("\n With every 'n' the time taken and iterations will increase")
