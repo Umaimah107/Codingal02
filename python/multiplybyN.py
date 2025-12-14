8cf204eeb6a7f18d535d4ff7fd9e32ef237874ae
Umaimah <umaimahali07@gmail.com>
2025-12-14 11:53:51 +0500

    *

---
+++ python/multiplybyN.py
@@ -0,0 +1,12 @@
+def o1(n,m):
+    total = n*m
+    print("\n1 iteration: ", total)
+
+def oN(n,m):
+    total = 0
+    for i in range(1, n+1):
+        total += m
+    print("n iteration: ",total)
+
+m = int(input("Enter 'a' for a*b : "))
+n = int(input("Enter 'b' for a*b : "))
