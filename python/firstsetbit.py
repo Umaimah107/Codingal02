c33edca66945ffb239111d1c9b48b917a160c50b
Umaimah <umaimahali07@gmail.com>
2026-01-10 11:48:15 +0500

    first set bit

---
+++ python/firstsetbit.py
@@ -0,0 +1,11 @@
+def firstSetBit(number):
+    position = 1
+    mask = 1
+
+    while (not(number & mask)):
+        mask = mask << 1
+        position+=1 
+    return position
+
+number = int(input("Enter number : "))
+print("Position of the first set bit : ", firstSetBit(number))
