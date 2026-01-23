cb80203f40740cbefb1aad650839c39c1ffe6ca3
Umaimah <umaimahali07@gmail.com>
2026-01-23 16:13:44 +0500

    swapping variables

---
+++ python/swappingwithnothirdvar.py
@@ -0,0 +1,13 @@
+def swap1(a,b):
+    a = a ^ b
+    b = a ^ b
+    a = a ^ b
+    print("After Swapping ; a= ",a,"b=", b)
+swap1(1,2)
+
+def swap2(a,b):
+    a = (a & b) + (a | b)
+    b = a+ ( ~b) + 1
+    a = a+ ( ~b) + 1
+    print("After Swapping ; a= ",a,"b=", b)
+swap2(3,4)
