80c0966faeb42ab075ccc0c8fab4d090ba918372
Umaimah <umaimahali07@gmail.com>
2026-02-22 12:17:05 +0500

    sql using python

---
+++ python/sql using python.py
@@ -0,0 +1,9 @@
+print("What is the type of above given database -\n 1)Relational Database \n 2)Non-Relational Database")
+answer = int (input("Enter your answer here..."))
+if answer == 2:
+    print("You guessed right")
+
+else:
+    print("Your guess is wrong.")
+
+print("\n Please tell your mentor why you guessed this?")
