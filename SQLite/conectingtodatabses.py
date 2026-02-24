424772b3c08747e37d4a82b9192043a7256fef39
Umaimah <umaimahali07@gmail.com>
2026-02-24 19:08:05 +0500

    database

---
+++ SQLite/conectingtodatabses.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+cursor = connection.cursor()
+cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
+
+tables = cursor.fetchall()
+
+print("Tables in the database: ")
+for table in tables:
+    print(table[0])
+
+connection.close()
